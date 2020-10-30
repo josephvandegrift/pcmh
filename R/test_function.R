ce2ec09841b69b6375ccbc69ac7067ffb8b88ffe
joseph.vandegrift <Joseph.VandeGrift@gdit.com>
2020-10-30 12:29:27 -0500

    Added placeholder for future function

---
+++ R/test_function.R
@@ -0,0 +1,17 @@
+#' Test Function
+#'
+#' Placeholder for future function
+#'
+#' @param x input
+#'
+#' @return Returns \code{x}.
+#' @export
+#'
+#' @examples
+#' test_function("hi")
+#' \dontrun{
+#' test_function(hi)
+#' }
+test_function <- function(x) {
+  return(x)
+}
