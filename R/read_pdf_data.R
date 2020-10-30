eca39929df08a7d88b88f6ce8d40246d6a0c05d6
joseph.vandegrift <Joseph.VandeGrift@gdit.com>
2020-10-30 14:07:03 -0500

    Updated read_pdf_data function

--- R/read_pdf_data.R
+++ R/read_pdf_data.R
@@ -1,8 +1,10 @@
 #' Test Function
 #'
-#' Placeholder for future function
+#' Uses \code{\link[fs]{dir_ls}} to create a \code{list} of the \code{.pdf} PCMH
+#'   Report filepaths. Then uses \code{\link[furrr]{future_map}} to map
+#'   \code{\link[pdftools]{pdf_data}} to read in each PCMH Report.
 #'
-#' @param path Filepath to the \code{.pdf} PCMH Reports
+#' @param path Filepath to a directory of  \code{.pdf} PCMH Reports.
 #'
 #' @return Returns a \code{list} of \code{lists}. Each \code{element} of the
 #'   \code{list} refers to a PCMH Report and is a \code{list} of \code{dataframes},
@@ -10,11 +12,14 @@
 #' @export
 #'
 #' @importFrom fs dir_ls
+#' @importFrom furrr future_map
+#' @importFrom pdftools pdf_data
 #' @examples
 #' \dontrun{
 #' read_pdf_data(hi)
 #' }
 read_pdf_data <- function(path) {
-  files <-
-    fs::dir_ls(path)
+  files <- fs::dir_ls(path)
+  out <- furrr::future_map(files, pdftools::pdf_data)
+  return(out)
 }
