dfa9f0ff489b113b1b2fcc4fb628a08a8c894bcf
joseph.vandegrift <Joseph.VandeGrift@gdit.com>
2020-11-09 08:51:12 -0600

    Added the import_pdf_data function and documentation

---
+++ R/import_pdf_data.R
@@ -0,0 +1,23 @@
+#' Import pdf_data
+#'
+#' This function is a wrapper around \code{\link[pcmh]{read_pdf_data}} and
+#'   \code{\link[pcmh]{clean_pdf_data}}.
+#'   This function reads in the \code{.pdf} PCMH reports and cleans the data
+#'   for future analysis.
+#'
+#' @param .directory A path to a directory containing the \code{.pdf} PCMH
+#'   reports.
+#'
+#' @return Returns a \code{list} of \code{dataframes}. One for each \code{.pdf}
+#'   PCMH report in the directory.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' import_pdf_data(~Documents)
+#' }
+import_pdf_data <- function(.directory) {
+  out <- pcmh::read_pdf_data(.directory)
+  out <- furrr::future_map(out, pcmh::clean_pdf_data)
+  return(out)
+}
