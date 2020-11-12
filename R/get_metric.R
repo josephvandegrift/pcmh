ae80c7b5f301734b3e2cf008aea7f0526ca72676
joseph.vandegrift <Joseph.VandeGrift@gdit.com>
2020-11-12 08:51:01 -0600

    Updated .get_metric and .read_oracle_data functions/docs

--- R/get_metric.R
+++ R/get_metric.R
@@ -3,6 +3,7 @@
 #' This function is a wrapper around \code{\link[pcmh]{.filter_pdf_data}} and
 #'   extracts the specific \code{.metric} input.
 #'
+#' @param .data_frame A \code{dataframe} you wish to extract data from.
 #' @param .metric Input for which metric to extract.
 #'
 #' @return Returns a \code{tibble} of the specified \code{.metric}.
@@ -14,8 +15,15 @@
 #' \dontrun{
 #' .get_metric(.metric)
 #' }
-.get_metric <- function(.metric) {
-  .params <- c(1, 2, 3, 4)
-  out <- pcmh::.filter_pdf_data(.params)
+.get_metric <- function(.data_frame, .metric) {
+  .params <- list(
+    .data_frame = .data_frame,
+    .page = 4,
+    x_min = 1,
+    x_max = 10,
+    y_min = 1,
+    y_max = 10
+  )
+  out <- do.call(pcmh::.filter_pdf_data, .params)
   return(tibble::as_tibble(out))
 }
--- R/read_oracle_data.R
+++ R/read_oracle_data.R
@@ -1,18 +1,21 @@
 #' Read Oracle Data
 #'
-#' This function reads in the \code{.csv} oracle data to be checked against the
-#'   \code{.pdf} PCMH report data.
+#' This function uses \code{\link[vroom]{vroom}} to read in the \code{.csv}
+#'   oracle data to be checked against the \code{.pdf} PCMH report data.
 #'
 #' @param .path Filepath to the oracle data.
 #' @param ... Extra parameters to be passed to \code{\link[vroom]{vroom}}.
 #'
-#' @return
+#' @return Returns a \code{tibble} of the \code{.csv} oracle data
 #' @export
 #'
 #' @importFrom vroom vroom
 #' @importFrom tibble as_tibble
 #'
 #' @examples
+#' \dontrun{
+#' .read_oracle_data(.path)
+#' }
 .read_oracle_data <- function(.path, ...) {
   out <- vroom::vroom(.path, ...)
   return(tibble::as_tibble(out))
