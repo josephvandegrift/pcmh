5e5167d89dbef1fd9fef56e8a746b60ad5f3d26c
joseph.vandegrift <Joseph.VandeGrift@gdit.com>
2020-11-17 08:43:55 -0600

    Added pcmh_qa function/doc

---
+++ R/pcmh_qa.R
@@ -0,0 +1,24 @@
+#' PCMH QA
+#'
+#' This function is a wrapper around \code{\link[pcmh]{import_pdf_data}} and
+#'   \code{\link[pcmh]{import_metric_data}}.
+#'   You call this function to generate output for the PCMH report QA.
+#'
+#' @param .path_pdf Path to the \code{directory} containing the \code{.pdf} PCMH
+#'   reports to be passed to \code{\link[pcmh]{import_pdf_data}}.
+#' @param .path_metric Path to the \code{.csv} metric data to be passed to
+#'   \code{\link[pcmh]{import_metric_data}}.
+#' @param .path_out Path to the location the output will be saved.
+#'
+#' @return Retrurns the character string "You completed PCMH QA!"
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' pcmh_qa(.path_pdf, .path_metric, .path_out)
+#' }
+pcmh_qa <- function(.path_pdf, .path_metric, .path_out) {
+  pdf_data <- pcmh::import_pdf_data(.path_pdf)
+  metric_data <- pcmh::import_metric_data(.path_metric)
+  return(paste0("You completed PCMH QA!"))
+}
