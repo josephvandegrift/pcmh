75deb54f22a0a9266fbc28a1ad6d6b057beae72e
joseph.vandegrift <Joseph.VandeGrift@gdit.com>
2020-11-19 11:42:11 -0600

    Updated .checks function/docs

--- R/checks.R
+++ R/checks.R
@@ -7,6 +7,7 @@
 #' @param .metric_data Output from \code{\link[pcmh]{import_metric_data}}.
 #' @param .prvdr_num Specific provider number.
 #' @param .metric Specific metric to be checked.
+#' @param .page Specific page number the metric is on.
 #'
 #' @return Returns a \code{tibble} of \code{TRUE} if the value matched the data
 #'   from oracle or a \code{FALSE} if it did not.
@@ -19,13 +20,15 @@
 #' \dontrun{
 #' .checks(.metrics, .metric_data, .prvdr_num, .page, .metric)
 #' }
-.checks <- function(.metrics, .metric_data, .prvdr_num, .metric) {
+.checks <- function(.metrics, .metric_data, .prvdr_num, .metric, .page) {
   df1 <- dplyr::filter(.metrics,
                        .metrics$prvdr_num == .prvdr_num,
-                       .metrics$metric_var == .metric)
+                       .metrics$metric_var == .metric,
+                       .metrics$page == .page)
   df2 <- dplyr::filter(.metric_data,
                        .metric_data$prvdr_num == .prvdr_num,
                        .metric_data$mtrc_cd == .metric)
   out <- df1[c("dnmtr_num", "nmrtr_num")] == df2[c("dnmtr_num", "nmrtr_num")]
+  out <- cbind(df1[1:5], out)
   return(tibble::as_tibble(out))
 }
