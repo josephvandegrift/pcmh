cf1e1dc99c543e90facd532c532eaeeedebbd54c
joseph.vandegrift <Joseph.VandeGrift@gdit.com>
2020-12-15 09:02:51 -0600

    Added .import_cc_data function/doc

---
+++ R/import_cc_data.R
@@ -0,0 +1,21 @@
+#' Import Care Category Data
+#'
+#' Reads and cleans the Care Category data.
+#'
+#' @param path \code{Filepath} to directory containing care category \code{.xlsx}.
+#' @param ... Extra parameters to be passed to \code{\link[pcmh]{.read_metric_data}}.
+#'
+#' @return Returns a \code{tibble}
+#' @export
+#'
+#' @importFrom tibble as_tibble
+#'
+#' @examples
+#' \dontrun{
+#' .import_cc_data(path, ...)
+#' }
+.import_cc_data <- function(path, ...) {
+  out <- pcmh::.read_metric_data(path, ...)
+
+  return(tibble::as_tibble(out))
+}
